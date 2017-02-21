9ef720b2f0db70e1351e8fe65b6d0c776c7e9e1c
Mathias <Mathias@Mathias-Pc>
2017-02-21 19:52:36 +0100

    Add project files.

---
+++ I4SWD-HandIn1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWD_HandIn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

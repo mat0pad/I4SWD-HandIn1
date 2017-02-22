7973ddfb66779cc3013bf85eba5e145937f4160d
Anton <antonzxcz@yahoo.dk>
2017-02-22 10:15:58 +0100

    portfoliodisplay2

--- I4SWD-HandIn1/Portfolio.cs
+++ I4SWD-HandIn1/Portfolio.cs
@@ -8,14 +8,10 @@ namespace I4SWD_HandIn1
 {
     public class Portfolio : IPortfolio
     {
-<<<<<<< HEAD
 
         private IPortfolioDisplay Display;
         public List<Stock> Stocks = new List<Stock>();
-=======
-        public IPortfolioDisplay Display;
-        private List<Stock> Stocks = new List<Stock>();
->>>>>>> fccd0c022c01809bf23cd52a04c0113b8a4a29cd
+
         public double TotalValue { get; set; } = 0;
         public string Name { get; set; }
 

6bdf399c23054de17e61eac584a275c0d95008ab
Mathias <Mathias@Mathias-Pc>
2017-02-22 08:55:03 +0100

    added portfolio

---
+++ I4SWD-HandIn1/Portfolio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWD_HandIn1
+{
+    public class Portfolio : IPortfolio, IPortfolioDisplay
+    {
+
+        public void PrintPortfolio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

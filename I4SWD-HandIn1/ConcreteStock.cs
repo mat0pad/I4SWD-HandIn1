6dd80def7976bce74444c8e2a4439d96308ac549
Anton <antonzxcz@yahoo.dk>
2017-02-22 08:51:56 +0100

    implementet ConcreteSubject

---
+++ I4SWD-HandIn1/ConcreteStock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWD_HandIn1
+{
+    class ConcreteStock :  Stock
+    {
+        public double stackOfstock { get; set; }
+        public string name { get; set; }
+        public double amount { get; set; }
+
+        public void SetStackOfstock(double StackOfStock)
+        {
+            this.stackOfstock = StackOfStock;
+            Notify();
+        }
+        public void SetName(string Name)
+        {
+            name = Name;
+            Notify();
+        }
+        public void SetAmount(double Amount)
+        {
+            amount = Amount;
+            Notify();
+        }
+        Stock GetState()
+        {
+            return this;
+        }
+
+
+    }
+}
